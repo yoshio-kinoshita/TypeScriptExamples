cafb914a3582565be4ec79aef1ed29819fe04afc
木下喜雄 <kinoshitayoshio@kinoshitayoshionoMacBook-Air.local>
2023-03-11 14:32:34 +0900

    add new files

---
+++ ch3/array_basic.js
@@ -0,0 +1,6 @@
+{
+    var data1 = ['java', 'c#', 'python'];
+    var data2 = ['java', 'c#', 'python'];
+    console.log(data1[0]);
+    console.log(data2[1]);
+}
---
+++ ch3/array_basic.ts
@@ -0,0 +1,6 @@
+{
+    let data1: string[] = ['java', 'c#', 'python'];
+    let data2: Array<string> = ['java', 'c#', 'python'];
+    console.log(data1[0]);
+    console.log(data2[1]);
+}
